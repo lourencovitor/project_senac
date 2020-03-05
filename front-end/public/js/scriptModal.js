c0eef4f7f297466cc103a2759de05e3ea376f141
PEDRO HENRIQUE GONZALES BERNADINI <pedro.hgbernadini@senacsp.edu.br>
2020-03-05 20:19:16 -0300

    Mudou Var para Let

--- front-end/public/js/scriptModal.js
+++ front-end/public/js/scriptModal.js
@@ -1,10 +1,7 @@
-var modal = document.getElementById("myModal");
-//var modalReg = document.getElementById("modalReg");
-var btn = document.querySelector("#entrar");
-//var btn2 = document.querySelector("#registrar");
-//var btn3 = document.querySelector("#registrar2");
-var span = document.getElementsByClassName("close-modal")[0];
-//var span2 = document.getElementsByClassName("close-modal2")[0];
+let modal = document.getElementById("myModal");
+let btn = document.querySelector("#entrar");
+let span = document.getElementsByClassName("close-modal")[0];
+
 
 // MODAL
 btn.addEventListener("click", function(){
@@ -18,12 +15,6 @@ modal.classList.add('sumir-modal');
 modal.classList.remove('aparecer-modal');
 })
 
-// //CAIXA para Fechar no X
-// span2.addEventListener("click", function() {
-//   modalReg.classList.add('sumir-modalRegistro');
-//   modalReg.classList.remove('aparecer-modalRegistro');
-//   })
-
 // Fechar modal, no target do modal
 window.addEventListener("click", function(event) {
   if (event.target == modal) {
@@ -31,25 +22,3 @@ window.addEventListener("click", function(event) {
     modal.classList.remove('aparecer-modal');
   }
 })
-
-// // Fechar modal, no target do modal
-// window.addEventListener("click", function(event) {
-//   if (event.target == modalReg) {
-//     modalReg.classList.add('sumir-modalRegistro');
-//     modalReg.classList.remove('aparecer-modalRegistro');
-//   }
-// })
-
-// btn2.addEventListener("click", function(){
-//   modalReg.classList.add('aparecer-modalRegistro');
-//   modalReg.classList.remove('sumir-modalRegistro');
-//   modal.classList.add('sumir-modal');
-//   modal.classList.remove('aparecer-modal');
-// })
-
-// btn3.addEventListener("click", function(){
-//   modalReg.classList.add('aparecer-modalRegistro');
-//   modalReg.classList.remove('sumir-modalRegistro');
-//   modal.classList.add('sumir-modal');
-//   modal.classList.remove('aparecer-modal');
-// })
